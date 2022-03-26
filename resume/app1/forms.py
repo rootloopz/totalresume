590a4439be8b81ed5f2b92eb693b25b63d644e1d
ydoco <cwillard1@mail.csuchico.edu>
2022-03-26 14:41:15 -0700

    moved form from views into forms.py and editied models

--- resume/app1/forms.py
+++ resume/app1/forms.py
@@ -1,6 +1,35 @@
 from django import forms
 from django.core import validators
 from django.contrib.auth.models import User
+from .models import Resume
+
+
+#this is the form for creating a new resume
+class CreateMasterResume(forms.Form):
+    #education information
+    Syear = forms.DateField(label="start year")#start year
+    Eyear =  forms.DateField(label="enter your expected graduation year")#end year
+    school = forms.CharField(label="School name:")
+    gpa = forms.FloatField()
+    academAchieve = forms.CharField(label="enter your achievements", widget=forms.Textarea)
+
+    #job information
+    jobSyear = forms.DateField(label="start year")
+    jobEyear =  forms.DateField(label="enter your end year or none if it is your current job")
+    employerName = forms.CharField(label="Name of your employer")
+    location = forms.CharField(label="location of employer")
+
+    #skills section
+    skills = forms.CharField(label="skills")
+
+    #awards / extracariculars
+    awards = forms.CharField(label= "awards")
+    extraCariculars = forms.CharField(label= "enter any extracariculars")
+
+    #project section
+    projectName = forms.CharField(label="project name")
+    projectDesc = forms.CharField(label="description")
+
 
 class LoginForm(forms.Form):
     username = forms.CharField()
