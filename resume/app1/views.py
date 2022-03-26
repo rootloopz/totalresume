9749141a8ee761b841cf0ba3f613105bf6ad48d2
pswd1 <ctwilson1@csuchico.edu>
2022-03-26 09:50:14 -0700

    implemented home page within app1 and set it as the default view

--- resume/app1/views.py
+++ resume/app1/views.py
@@ -1,16 +1,45 @@
 from cProfile import label
+from tkinter import Label
 from unicodedata import name
 from django.shortcuts import render
 from django import forms
 
+#this is the form for creating a new resume
 class CreateMasterResume(forms.Form):
-    school = forms.CharField(label="School name:")#label="School name:")
-    gpa = forms.FloatField()#label="GPA")
+    #education information
+    Syear = forms.DateField(label="start year")#start year
+    Eyear =  forms.DateField(label="enter your expected graduation year")#end year
+    school = forms.CharField(label="School name:")
+    gpa = forms.FloatField()
+    academAchieve = forms.CharField(label="enter your achievements", widget=forms.Textarea)
 
+    #job information
+    jobSyear = forms.DateField(label="start year")
+    jobEyear =  forms.DateField(label="enter your end year or none if it is your current job")
+    employerName = forms.CharField(label="Name of your employer")
+    location = forms.CharField(label="location of employer")
+
+    #skills section
+    skills = forms.CharField(label="skills")
+
+    #awards / extracariculars
+    awards = forms.CharField(label= "awards")
+    extraCariculars = forms.CharField(label= "enter any extracariculars")
+
+    #project section
+    projectName = forms.CharField(label="project name")
+    projectDesc = forms.CharField(label="description")
+
+
+
+
+#this is the basic form for getting a username and password from a new user
 class NewUser(forms.Form):
     username = forms.CharField(label="desired user name",)
     password = forms.PasswordInput()
     confirmPassword = forms.PasswordInput()
+
+
 
 # Create your views here.
 def NewResume(request):
@@ -26,3 +55,6 @@ def GetNewUser(request):
     return render(request, "app1/NewUserForm.html",{
         "form": NewUser()
     })
+
+def homepage(request):
+    return render(request, "app1/welcome.html")
