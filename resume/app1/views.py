7f38ec87a5b4aea2734e9f334cc1b551a248555c
pswd1 <ctwilson1@csuchico.edu>
2022-03-25 22:10:05 -0700

    added some urls,some files and preped for major work on the forms page

--- resume/app1/views.py
+++ resume/app1/views.py
@@ -1,3 +1,17 @@
+from cProfile import label
+from unicodedata import name
 from django.shortcuts import render
+from django import forms
+
+class CreateMasterResume(forms.Form):
+    school = forms.TextInput()#label="School name:")
+    degree = forms.FloatField()#label="GPA")
+
 
 # Create your views here.
+def NewResume(request):
+    if request.method == "POST":
+        form = CreateMasterResume(request.POST)
+    return render(request, "app1/NewResumeForm.html",{
+        "form": CreateMasterResume()
+    })
