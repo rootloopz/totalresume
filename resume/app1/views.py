b23eea2238b32b130ee47f7483d993509919c24e
ydoco <cwillard1@mail.csuchico.edu>
2022-05-03 15:45:55 -0700

    removed pdf printing and related html and requirements

--- resume/app1/views.py
+++ resume/app1/views.py
@@ -1,3 +1,4 @@
+import io
 from unicodedata import name
 from django.shortcuts import render, redirect
 from django import forms
@@ -6,14 +7,8 @@ from django.contrib.auth.decorators import login_required
 from app1.forms import JoinForm, LoginForm, resumeForm
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
-
-import io
 from django.http import FileResponse
-from reportlab.pdfgen import canvas
-from reportlab.lib.units import inch
-from reportlab.lib.pagesizes import letter
 from app1.models import Resume
-
 
 # Create your views here.
 @login_required
@@ -78,129 +73,3 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return redirect("/")
-
-###Randall's Stuff###
-class Sub:
-    def __init__(self,title,titleSize,date,dateSize,desc,descSize):
-        self.title = title
-        self.titleSize = titleSize
-        self.date = date
-        self.dateSize = dateSize
-        self.desc = desc
-        self.descSize = descSize
-
-class Subject:
-    def __init__(self,title,entries):
-        self.title = title
-        self.entries = entries
-
-def drawRuler(pdf):
-    pdf.setLineWidth(1)
-    pdf.setStrokeColorRGB(0.54,0.54,0.54)
-    for x in range(0,610,100):
-        pdf.line(x,0,x,900) #draw vertical lines
-        pdf.drawString(x,10, 'x'+str(x))
-    for y in range(0,900,100):
-        pdf.line(0,y,610,y) #draw horizontal lines
-        pdf.drawString(10,y, 'y'+str(y))
-
-def GetPDF(request):
-    user1 = Resume.objects.get(user = request.user)
-    print(f"user =  {user1}")
-    fileName = 'SomePDFName.pdf'
-    resumeName = 'Randall Fowler'
-    contactInfo = '(530)420-6969'
-    #subject object
-    subject = user1.school
-    print(subject)
-    date = ['Fall 2019 - Spring 2022']
-    description = ['Bachelors of Science, Computer Science - California State University, Chico\nGraduation expected in Spring 2023\nChico GPA: 3.66\nDean\'s list Spring 2021, Fall 2021']
-    #description = "\n".join(wrap(description, 500))
-    placement = [[40,120]]
-    subjDividerY = [200]
-
-    # Education Subject Entries
-    EdSub1 = Sub(subject,14,user1.startYear,10,user1.academAchieve,12)
-    #Education = Subject("Education",[EdSub1,EdSub1])
-    ExSub1 = Sub(user1.employerName,14,user1.jobStartYear,10,user1.academAchieve,12)
-    SkSub1 = Sub(user1.skills,14,user1.startYear,10,user1.academAchieve,12)
-    ASub1 = Sub(user1.extraCariculars,14,user1.startYear,10,user1.academAchieve,12)
-    PSub1 = Sub(user1.projectName,14,user1.startYear,10,user1.academAchieve,12)
-    
-    Education = Subject("Education",[EdSub1])
-    Expierence = Subject("Expierence",[ExSub1])
-    Skills = Subject("Skills",[SkSub1])
-    Awards = Subject("Awards",[ASub1])
-    Projects = Subject("Projects",[PSub1])
-    subjects = [Education, Expierence,Skills,Awards,Projects]
-
-    #create byte stream buffer
-    buf = io.BytesIO()
-    #Create canvas
-    pdf = canvas.Canvas(buf, pagesize=letter,bottomup=0)
-
-    # 1) Draw Grid
-    #drawRuler(pdf)  #used for creating templates
-    
-    # 2) Top Portion
-    titleText = pdf.beginText()
-    titleText.setTextOrigin(200,50)
-    lines = [resumeName,contactInfo]
-    fonts = [20,10]
-    for i in range(len(lines)):
-        titleText.setFont("Helvetica",fonts[i])
-        titleText.textLine(lines[i])
-    pdf.drawText(titleText)
-
-    # 3) Divider from top
-    pdf.setStrokeColorRGB(0,0,0)
-    titleDivideLineY = 90
-    pdf.line(0,titleDivideLineY,630,titleDivideLineY)
-
-    # 4) Main content
-    i = 0
-    subjectTitleX = 200 #constant
-    subjectTitleY = 120
-    subTitleX = 40 #constant
-    subTitleY = subjectTitleY + 15
-    dateX = 440 #constant
-    #dateY should be same as subTitleY
-    descX = 40  #constant
-    descY = subTitleY + 10
-    subjectText = pdf.beginText()
-    for subject in subjects:
-        #Grab Subject: print title
-        subjectText.setFont("Helvetica",20)
-        subjectText.setTextOrigin(subjectTitleX,subjectTitleY)
-        subjectText.textLines(subject.title)
-        for sub in subject.entries:
-            #grab subject: print title, date, description
-            #print title
-            subjectText.setFont("Helvetica",sub.titleSize)
-            subjectText.setTextOrigin(subTitleX,subTitleY)
-            subjectText.textLines(sub.title)
-            #print date
-            subjectText.setFont("Helvetica",sub.dateSize)
-            subjectText.setTextOrigin(dateX,subTitleY)
-            subjectText.textLines(sub.date)
-            #print description
-            subjectText.setFont("Helvetica",sub.descSize)
-            subjectText.setTextOrigin(descX,descY)
-            subjectText.textLines(sub.desc)
-            #change iter for entry
-            subTitleY = descY + 50
-            descY = subTitleY + 10
-                #subTitleY = subTitleY + 70
-                #descY = subTitleY + 10
-        #subjectTitleY = descY + len(subject.entries)*80
-        subjectTitleY = descY
-        subTitleY = subjectTitleY + 15
-        descY = subTitleY + 10
-        #pdf.line(20,subjectTitleY-25,592,subjectTitleY-25)
-        pdf.drawText(subjectText)
-
-    pdf.setTitle(resumeName+'Resume')
-    pdf.showPage()
-    pdf.save()
-    buf.seek(0)
-    return FileResponse(buf, as_attachment=True, filename=fileName)
--- resume/resume/urls.py
+++ resume/resume/urls.py
@@ -24,6 +24,5 @@ urlpatterns = [
     path('login/', app1_views.user_login),
     path('logout/', app1_views.user_logout),
     path("resume/", app1_views.NewResume),
-    path('resume/submit', app1_views.NewResume),
-    path('create_pdf/', app1_views.GetPDF)
+    path('resume/submit', app1_views.NewResume)
 ]
