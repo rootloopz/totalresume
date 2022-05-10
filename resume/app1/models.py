aa87da5011a77b01eaf19690f55176d7b395f503
ydoco <cwillard1@mail.csuchico.edu>
2022-05-10 13:41:32 -0700

    changed model and created new fill out form page

--- resume/app1/models.py
+++ resume/app1/models.py
@@ -2,27 +2,30 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Resume(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    #contact info, name and more
+    name = models.CharField(max_length=30)
+    number = models.CharField(max_length=12)
+    email = models.EmailField()
     #education information
     startYear = models.CharField(max_length = 4)#start year
-    endYear =  models.CharField(max_length = 4)#end year
+    endYear =  models.CharField(max_length = 4, blank=True)#end year
     school = models.CharField(max_length = 100)
-    gpa = models.CharField(max_length = 4)
-    academAchieve = models.CharField(max_length = 300)
+    gpa = models.CharField(max_length = 4, blank=True)
+    schoolextra = models.CharField(max_length = 300, blank=True)
 
     #job information
     jobStartYear = models.CharField(max_length = 10)
     jobEndYear =  models.CharField(max_length = 10)
-    employerName = models.CharField(max_length = 100)
-    location = models.CharField(max_length = 100)
+    employer = models.CharField(max_length = 100)
+    location = models.CharField(max_length = 100, blank=True)
 
     #skills section
-    skills = models.CharField(max_length = 500)
+    skills = models.CharField(max_length = 500, blank=True)
 
     #awards / extracariculars
-    awards = models.CharField(max_length = 500)
-    extraCariculars = models.CharField(max_length = 1000)
+    awards = models.CharField(max_length = 500, blank=True)
+    extraCariculars = models.CharField(max_length = 1000, blank=True)
 
     #project section
-    projectName = models.CharField(max_length = 100)
-    projectDesc = models.CharField(max_length = 1000)
+    projectName = models.CharField(max_length = 100, blank=True)
+    projectDesc = models.CharField(max_length = 1000, blank=True)
